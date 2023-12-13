1cd627ce83d9730d617a15c17d595b281e04c5a1
Kawan Serafim <kawanserafimdesouza@hotmail.com>
2023-12-12 22:38:39 -0300

    Desafio-Smartphone_v1

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(){}
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){}
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Page R$ 10,00 para instalar... rs");
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+            if (valor == 10)
+            {
+                Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone...");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:\n");
+Nokia nokia = new Nokia(numero: "1234", modelo: "modelo_1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:\n");
+Iphone iphone = new Iphone(numero: "123456", modelo: "modelo_7", imei: "222222", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Youtube");
+iphone.ReceberLigacao();
